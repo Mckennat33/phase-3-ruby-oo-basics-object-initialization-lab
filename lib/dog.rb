8aaaafaa53eb78e7873dfa50362036ae965d73cd
Thomas <thomasmckenna12gmail.com>
2022-08-29 18:58:47 -0500

    completed - error with the test code

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+Class Dog
+    attr_reader : name
+    attr_reader : breed
+
+    def initialize(name)
+        @name = name
+    end
+
+    def initialize(breed)
+        @breed = breed
+    end 
+
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person 
+    attr_reader :name
+    def name(name)
+        @name = name 
+    end
+
+end
